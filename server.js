d9622ae8bba7c9bfdc0849567f356b3dead4161d
Jacob Christensen <jacobchristensen811@gmail.com>
2025-05-13 22:32:59 -0600

    added global error-handling middleware to handle server errors

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ app.use(express.json());
 
 // Use the routes
 app.use('/getToken', getTokenRoute);
-
 app.use('/cards', cardsRoute);
+
+// Global error-handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Log the error stack trace for debugging
+  res.status(500).json({ errorMessage: 'An internal server error occurred.' });
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
